4107dd670f03393dcaae63fe816260abfec3538f
Rohit Funde <rohit@ventii.jp>
2023-09-22 18:11:45 +0900

    added

---
+++ ObjectDetection-CoreML/Device/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Spike Essential Connector
+//
+//  Created by Rohit Funde on 2023/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        VStack(spacing: 32) {
+            HStack() {
+                if viewModel.isScanning {
+                    Button("Scanning...") {
+                        viewModel.cancel()
+                    }
+                } else {
+                    if let name = viewModel.peripheral?.name {
+                        Button(action: { viewModel.disconnect() }) {
+                            Image(systemName: "xmark.circle.fill")
+                        }
+                        Text(name)
+                    } else {
+                        Button("Connect") {
+                            viewModel.connect()
+                        }
+                    }
+                }
+            }
+            
+            Group {
+                Text("\(viewModel.power)")
+                
+                HStack {
+                    Button(action: { viewModel.decrement() }) {
+                        Image(systemName: "minus.circle")
+                    }
+                    Button(action: { viewModel.stop() }) {
+                        Image(systemName: "stop.circle")
+                    }
+                    Button(action: { viewModel.increment() }) {
+                        Image(systemName: "plus.circle")
+                    }
+                }
+            }
+            .font(.system(size: 64))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
